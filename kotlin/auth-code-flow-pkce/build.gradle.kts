4f033bdfa843aca29cae20d68cdc0cb660ce24cd
Arkady.Bazhanov <arkady.bazhanov@gmail.com>
2023-06-12 15:41:57 +0200

    Add sample demonstrating Authorization Code Flow with PKCE

---
+++ kotlin/auth-code-flow-pkce/build.gradle.kts
@@ -0,0 +1,35 @@
+val ktor_version: String by project
+val logback_version: String by project
+val space_sdk_version: String by project
+val jackson_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.8.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/space/maven")
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-html-builder:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jackson_version")
+    implementation("org.jetbrains:space-sdk-jvm:$space_sdk_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+}
